00259d7674b45ea44d96df64550ed2e835380e55
m0opha <jsaguamoraga@gmail.com>
2025-06-21 05:22:35 -0400

    modified:   src/modules/connect_device.py

--- src/modules/connect_device.py
+++ src/modules/connect_device.py
@@ -1,20 +1,25 @@
 import subprocess
 
 from .colors import pSuccess, pError
+import subprocess
 
-def connect(mac_address, device_name):
-    command = ["bluetoothctl" , "connect", mac_address]
-    result = subprocess.run(
-        command,
-        text=True,
-        capture_output=True
-    )
+def connect(mac_address, device_name, timeout=30):
+    command = ["bluetoothctl", "connect", mac_address]
+    try:
+        result = subprocess.run(
+            command,
+            text=True,
+            capture_output=True,
+            timeout=timeout
+        )
+    except subprocess.TimeoutExpired:
+        pError(f"[-] Timeout connecting to {device_name}")
+        return False
 
     if result.returncode != 1:
         pSuccess(f"[+] Connected to ", no_salt=True)
         print(device_name)
         return True
-    
-    pError(f"[-] Failed to connect to ", no_salt=True) 
-    print(device_name)
+
+    pError(f"[-] Failed to connect to ", no_salt=True)
     return False
